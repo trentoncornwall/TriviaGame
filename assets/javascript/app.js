f9c085d1cf09adb88af3719adae2bdc5b1e45656
Trenton Cornwall <trenton434@gmail.com>
2019-07-15 10:35:20 -0600

    choose a theme, horror, added 8 more questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,88 @@
 var questionOne = {
-	question: "This is first question, answer is 4",
-	choices: ["first choice", "second choice", "third choice", "forth choice"],
-	answer: "forth choice"
+	question:
+		"What classic horror movie features a serial killer in a William Shatner mask?",
+	choices: [
+		"friday the 13th",
+		"halloween",
+		"texas chainsaw massacre",
+		"the fog"
+	],
+	answer: "halloween"
 };
 
 var questionTwo = {
-	question: "This is the first question, answer 2",
-	choices: ["FIRST CHOICE", "SECOND CHOICE", "THIRD CHOICE", "FORTH CHOICE"],
-	answer: "SECOND CHOICE"
+	question:
+		"In what horror movie does the protagonist write a book that contains only the line 'All work and no play makes Jack a dull boy' repeated over and over and over?",
+	choices: ["the shining", "the devil's rain", "writer's block", "room 1408"],
+	answer: "the shining"
 };
 
+var questionThree = {
+	question: "What was the original title of The Blob?",
+	choices: ["the lump", "the glob", "the goop", "the creature"],
+	answer: "the glob"
+};
+
+var questionFour = {
+	question:
+		"What horror film caused some theatres to suggest that patrons prone to motion sickness sit in the aisle seats?",
+	choices: [
+		"vertigo",
+		"mimic",
+		"friday the 13th, part iii: 3d",
+		"the blair witch project"
+	],
+	answer: "the blair witch project"
+};
+
+var questionFive = {
+	question: "What was leatherface's choice weapon?",
+	choices: ["knife", "axe", "chainsaw", "bat"],
+	answer: "chainsaw"
+};
+
+var questionSix = {
+	question:
+		"What was the name that the demon from 'The Exorcist' used to contact Regan?",
+	choices: [
+		"captain spaulding",
+		"captain jack",
+		"captain howdy",
+		"captain harry"
+	],
+	answer: "captain howdy"
+};
+
+var questionSeven = {
+	question: "What is the name of the camp in Friday the 13th?",
+	choices: [
+		"camp mooselake",
+		"camp crystal lake",
+		"camp waziyata",
+		"camp timber lake"
+	],
+	answer: "camp crystal lake"
+};
+
+var questionEight = {
+	question: "What was the name of the killer clown in Stephen King's 'IT'?",
+	choices: ["pennywise", "pumpernickel", "monster", "clowny"],
+	answer: "pennywise"
+};
+
+var questionNine = {
+	question:
+		"How many people associated with 'The Exorcist' died during the film's year-long shoot?",
+	choices: ["none", "9 people", "5 people", "2 people"],
+	answer: "9 people"
+};
+
+var questionTen = {
+	question:
+		"Which one of these famous slashers has accumulated the highest body count?",
+	choices: ["freddy krueger", "michael myers", "jason voorhees", "leatherface"],
+	answer: "jason voorhees"
+};
 function timerStart(secs) {
 	var intervalID;
 	clearInterval(intervalID);
@@ -44,7 +117,18 @@ var board = {
 	losingImage: "WRONG ANSWER",
 	outOfTimeImage: "OUT OF TIME",
 
-	questions: [questionOne, questionTwo],
+	questions: [
+		questionOne,
+		questionTwo,
+		questionThree,
+		questionFour,
+		questionFive,
+		questionSix,
+		questionSeven,
+		questionEight,
+		questionNine,
+		questionTen
+	],
 	onQuestion: 0,
 
 	drawBoard: function() {
@@ -70,7 +154,7 @@ var board = {
 
 			//*MOVE TO NEXT QUESTION
 			this.onQuestion++;
-			timerStart(10);
+			// timerStart(10);
 			waitClick();
 		} else {
 			// TODO include a reset button
